b106aaa93638dc63e40faf5b784746cf54958ff9
andy <oyh129@163.com>
2020-03-31 23:15:36 +0800

    lesson6-goroutine的runtime方法补充

--- src/code.oldboy.com/studygolang/lesson6/02_goroutine_runtime方法补充/main.go
+++ src/code.oldboy.com/studygolang/lesson6/02_goroutine_runtime方法补充/main.go
@@ -10,61 +10,60 @@ import (
 // runtime.Gosched() ， runtime.Goexit()，runtime.GOMAXPROCS()
 func main() {
 
-	// test1()   
+	// test1()
 
 	// test2()
 
 	test3()
 }
 
-// 测试 runtime.Gosched() 
-func test1(){
+// 测试 runtime.Gosched()
+func test1() {
 
 	go func(s string) {
-		for i:=0;i<20;i++ {
+		for i := 0; i < 20; i++ {
 			fmt.Println(s)
 		}
 	}("world")
 
-	// 
-	for i:=0;i<20;i++ {
-		runtime.Gosched()   // 出让CPU 时间片,再次分配任务 (分配给其它 任务)
+	//
+	for i := 0; i < 20; i++ {
+		runtime.Gosched() // 出让CPU 时间片,再次分配任务 (分配给其它 任务)
 		fmt.Println("hello")
 	}
 
-
-	/*   打印信息：  
-		world
-		world
-		world
-		world
-		world
-		world
-		world
-		world
-		world
-		world
-		hello
-		world
-		hello
+	/*   打印信息：
+	world
+	world
+	world
+	world
+	world
+	world
+	world
+	world
+	world
+	world
+	hello
+	world
+	hello
 	*/
 
 	// 总结：
 	/*
-		打印 "hello" 的主协程，出让CPU 时间片给其它任务， 打印 "world"的goroutine 获得时间片开始打印, 
+		打印 "hello" 的主协程，出让CPU 时间片给其它任务， 打印 "world"的goroutine 获得时间片开始打印,
 		但是 主协程 并不是挂起，并不是等待其它 goroutien 执行完毕。 而是动态再切换回来，让主协程 与 其它goroutine 交互打印。
 	*/
-	
+
 }
 
 // 测试 runtime.Goexit()
-func test2(){
+func test2() {
 	go func() {
 		defer fmt.Println("A defer")
-		func (){
+		func() {
 			defer fmt.Println("B defer")
 			// 结束协程
-		    runtime.Goexit()
+			runtime.Goexit()
 			fmt.Println("B")
 		}()
 		fmt.Println("A")
@@ -88,9 +87,9 @@ func test2(){
 */
 
 // 测试 runtime.GOMAXPROCS()
-func test3(){
+func test3() {
 
-	runtime.GOMAXPROCS(3)   // 设置CPU 核心数，可以让程序 交互打印处理的更频繁
+	runtime.GOMAXPROCS(3) // 设置CPU 核心数，可以让程序 交互打印处理的更频繁
 	for {
 		go fmt.Print(0)
 		fmt.Print(1)
